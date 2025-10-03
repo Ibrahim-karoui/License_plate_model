93af83644f77521d3a92396ed1788640223c6a7a
Ibrahim karoui <brahimkaroui24@gmail.com>
2025-10-03 19:04:29 +0100

    update requirements file

--- src/temp.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print(torch.__version__)         # affiche la version de PyTorch
-print(torch.version.cuda)        # affiche la version de CUDA supportée
-print(torch.cuda.is_available()) # True si ton GPU est détecté
